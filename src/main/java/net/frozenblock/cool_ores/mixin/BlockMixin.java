abb25f05112e8c5fad74df6707ea4b06d70abc88
LiukRast <darthlucavader@gmail.com>
2024-12-19 17:55:40 +0100

    Code update + fixed schedule

--- src/main/java/net/frozenblock/cool_ores/mixin/BlockMixin.java
+++ src/main/java/net/frozenblock/cool_ores/mixin/BlockMixin.java
@@ -1,5 +1,9 @@
 package net.frozenblock.cool_ores.mixin;
 
+import io.github.foundationgames.automobility.automobile.AutomobileEngine;
+import io.github.foundationgames.automobility.automobile.AutomobileFrame;
+import io.github.foundationgames.automobility.automobile.AutomobilePrefab;
+import io.github.foundationgames.automobility.automobile.AutomobileWheel;
 import net.frozenblock.cool_ores.CoolOres;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -10,6 +14,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -18,6 +23,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +37,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BlockMixin {
     @Unique
     private static final TagKey<Block> ORE_TAG = TagKey.create(Registries.BLOCK, new ResourceLocation("forge:ores"));
+
+    @Unique
+    private static final AutomobilePrefab[] cool_ores$PREFABS = {
+            new AutomobilePrefab(
+                    new ResourceLocation("automobility:red_tractor"),
+                    AutomobileFrame.RED_TRACTOR,
+                    AutomobileWheel.TRACTOR,
+                    AutomobileEngine.COPPER
+            ),
+            new AutomobilePrefab(
+                    new ResourceLocation("automobility:yellow_tractor"),
+                    AutomobileFrame.YELLOW_TRACTOR,
+                    AutomobileWheel.TRACTOR,
+                    AutomobileEngine.COPPER
+            ),
+            new AutomobilePrefab(
+                    new ResourceLocation("automobility:green_tractor"),
+                    AutomobileFrame.GREEN_TRACTOR,
+                    AutomobileWheel.TRACTOR,
+                    AutomobileEngine.COPPER
+            ),
+            new AutomobilePrefab(
+                    new ResourceLocation("automobility:blue_tractor"),
+                    AutomobileFrame.SHOPPING_CART,
+                    AutomobileWheel.STEEL,
+                    AutomobileEngine.STONE
+            )
+    };
+
     @Inject(method = "playerDestroy", at = @At("TAIL"))
     private void cool_ores$playerDestroy(Level level, Player player, BlockPos pos, BlockState blockState, BlockEntity blockEntity, ItemStack itemStack, CallbackInfo ci) {
         if (level.isClientSide) return;
@@ -41,6 +77,12 @@ public class BlockMixin {
             block.playerDestroy(level, player, pos, block.defaultBlockState(), blockEntity, itemStack);
         } else if (blockState.getBlock() == Blocks.MAGENTA_GLAZED_TERRACOTTA) {
             cool_ores$Mine3x3(level, pos, player);
+        } else if(blockState.getBlock() == Blocks.LIGHT_BLUE_GLAZED_TERRACOTTA) {
+            final double random = Math.random();
+            final var randomCar = cool_ores$PREFABS[(int)(random*cool_ores$PREFABS.length)];
+            ItemEntity itemEntity = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, randomCar.toStack());
+            itemEntity.setPickUpDelay(10);
+            level.addFreshEntity(itemEntity);
         }
     }
 
@@ -66,7 +108,11 @@ public class BlockMixin {
                 BlockPos pos2 = pos.offset(x, 0, z);
                 BlockState state2 = level.getBlockState(pos2);
                 if (state2.is(ORE_TAG)){
-                    var list = state2.getDrops(new LootParams.Builder((ServerLevel) level));
+                    var list = state2.getDrops(
+                            new LootParams.Builder((ServerLevel) level)
+                                    .withParameter(LootContextParams.TOOL, player.getMainHandItem())
+                                    .withParameter(LootContextParams.ORIGIN, pos.getCenter())
+                    );
                     list.forEach(player::addItem);
                     level.setBlock(pos2, Blocks.AIR.defaultBlockState(), 3);
                 } else {
