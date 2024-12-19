0eb95de305418912f81a2e307d1f07e9340c80bc
LiukRast <darthlucavader@gmail.com>
2024-12-19 22:13:54 +0100

    UPDATE

--- src/main/java/net/frozenblock/cool_ores/mixin/BlockMixin.java
+++ src/main/java/net/frozenblock/cool_ores/mixin/BlockMixin.java
@@ -35,7 +35,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(Block.class)
 
 public class BlockMixin {
