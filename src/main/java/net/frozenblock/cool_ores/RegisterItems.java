5fad7264b5c0c39162226a23ebf0b7d935d769e6
LiukRast <darthlucavader@gmail.com>
2024-12-19 22:08:26 +0100

    UPDATE

--- src/main/java/net/frozenblock/cool_ores/RegisterItems.java
+++ src/main/java/net/frozenblock/cool_ores/RegisterItems.java
@@ -8,6 +8,7 @@ public class RegisterItems {
     public static final SwordItem DUMBBELLS = new SwordItem(Tiers.NETHERITE, 100, 100, new Item.Properties().stacksTo(1));
 
     public static void register(RegisterEvent.RegisterHelper<Item> helper) {
+
         helper.register("dumbbell", DUMBBELLS);
         helper.register("treadmill", new BlockItem(RegisterBlocks.TREADMILL_BLOCK, new Item.Properties()));
     }
